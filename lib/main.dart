555151089d3eaeac0fc4fd0776d529ee7dfcecbd
Osama-Saleh <osamasaleh509@gmail.com>
2025-07-21 21:48:46 +0300

    create varibale isPush

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:new_project/product_api_screen/ui/product_api_screen.dart';
 import 'package:new_project/train.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+   MyApp({super.key});
+   bool isPush = false;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
